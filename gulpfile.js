a51f46c3a771439b348276d8eadbb7197a3b73ad
André König <andre.koenig@posteo.de>
2014-10-09 17:19:11 +0200

    Written task for linting sources.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+/*
+ * mailbouncer
+ * 
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ *
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var sequence = require('run-sequence');
+
+var paths = {};
+
+paths.sources = ['./index.js', './gulpfile.js', './lib/**/*.js', './specs/**/*.js'];
+
+gulp.task('lint', function () {
+    return gulp.src(paths.sources)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('default', function (callback) {
+    return sequence('lint', callback);
+});
